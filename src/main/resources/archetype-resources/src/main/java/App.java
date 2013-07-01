52279b5ff6ac4ab3363ebdf517645205ab2987f3
Takashi AOKI <federkasten@me.com>
2013-07-01 21:43:36 +0900

    bump up version

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -4,29 +4,29 @@ import casmi.Applet;
 import casmi.AppletRunner;
 import casmi.KeyEvent;
 import casmi.MouseButton;
-import casmi.MouseEvent;
+import casmi.MouseStatus;
 import casmi.graphics.color.Color;
 import casmi.graphics.color.RGBColor;
 import casmi.graphics.element.Rect;
 
 /**
- * casmi Applet main class
+ * casmi main class.
  */
 public class App extends Applet {
 
     private Rect r;
     private Color c = new RGBColor("#0572B9");
-    
+
     @Override
     public void setup() {
         setSize(1024, 768);
         setFPS(30);
-        
-        // create rect element, set size, position and fill color, 
+
+        // create rect element, set size, position and fill color,
         r = new Rect(320, 240);
         r.setPosition(512, 384);
         r.setFillColor(c);
-        
+
         // add rect to rendering object tree
         addObject(r);
     }
@@ -34,32 +34,30 @@ public class App extends Applet {
     @Override
     public void update() {
         long value = System.currentTimeMillis();
-        
+
         // rotate rect
         double rot = value % (360 * 100);
         r.setRotation(rot / 100.0, 1.0, 2.0, 1.0);
-        
+
         // blink rect
         c.setAlpha((Math.sin(value / 400.0) + 1.0) / 2.0);
         r.setFillColor(c);
     }
 
-    // Comment out if you want to exec terminating processes.
-    // This is called when the application is quitted.
-//    @Override
-//    public void exit() {
-//        // Implement here.
-//    }
-
     @Override
-    public void mouseEvent(MouseEvent e, MouseButton b) {
+    public void exit() {
         // Implement here.
     }
 
     @Override
-    public void keyEvent(KeyEvent e) {
-        if (e == KeyEvent.PRESSED) {
-            if (getKeyCode() == 27) {  // if pressed ESC 
+    public void mouseEvent(MouseStatus status, MouseButton button) {
+        // Implement here.
+    }
+
+    @Override
+    public void keyEvent(KeyEvent event) {
+        if (event == KeyEvent.PRESSED) {
+            if (getKeyCode() == 27) {  // if pressed ESC
                 System.exit(0);
             }
         }
